79f8a46aae2ecbb0a498051487e43d342e3abcaf
Cédric <c.limosin@outlook.fr>
2024-12-18 15:49:48 +0100

    Création de la NavBar

---
+++ assets/JS/navbar.js
@@ -0,0 +1,7 @@
+// Mobile menu toggle script
+const menuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
