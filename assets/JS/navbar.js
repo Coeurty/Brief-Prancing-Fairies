c8cd3bf08582299ed3de47260a528aa1b2d179b6
Cédric <c.limosin@outlook.fr>
2024-12-18 16:44:12 +0100

    Ajout JS NavBar

--- assets/JS/navbar.js
+++ assets/JS/navbar.js
@@ -5,3 +5,19 @@ const mobileMenu = document.getElementById('mobile-menu');
 menuButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
+
+// JavaScript to toggle the dropdown menu
+const userMenuButton = document.getElementById('user-menu-button');
+const userMenu = document.getElementById('user-menu');
+
+userMenuButton.addEventListener('click', () => {
+    // Toggle the 'hidden' class to show or hide the dropdown menu
+    userMenu.classList.toggle('hidden');
+});
+
+// Close the dropdown menu when clicking outside
+document.addEventListener('click', (event) => {
+    if (!userMenuButton.contains(event.target) && !userMenu.contains(event.target)) {
+        userMenu.classList.add('hidden');
+    }
+});
