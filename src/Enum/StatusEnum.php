c733c538d264ae110d61ed45523906faf48f9c1a
glerique <gael.lerique@gmail.com>
2024-12-27 10:55:46 +0100

    feat: Added GetStatusAsString method

--- src/Enum/StatusEnum.php
+++ src/Enum/StatusEnum.php
@@ -6,4 +6,13 @@ enum StatusEnum: string
     case PENDING = 'pending';
     case PUBLISHED = 'published';
     case ARCHIVED = 'archived';
+
+    public function GetStatusAsString(): string
+    {
+        return match ($this) {
+            self::PENDING => 'En attente',
+            self::PUBLISHED => 'Publié',
+            self::ARCHIVED => 'Archivé',
+        };
+    }
 }
