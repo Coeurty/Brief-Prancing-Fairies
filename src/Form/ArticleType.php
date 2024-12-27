878b1882ddcba80d45a162cc062ab5b07e87dab4
glerique <gael.lerique@gmail.com>
2024-12-27 11:01:19 +0100

    feat: Add forms and templates for article management

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Article;
+use App\Entity\ArticleCategory;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('standFirst', TextType::class, [
+                'label' => 'Chapeau'
+            ])
+            ->add('coverImage', TextType::class, [
+                'label' => 'Image de couverture'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu'
+            ])
+            ->add('category', EntityType::class, [
+                'class' => ArticleCategory::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie'
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'nickname',
+                'label' => 'Auteur'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
