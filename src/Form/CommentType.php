41af689e304b7047733e565709f995a8dc8cfb7f
glerique <gael.lerique@gmail.com>
2024-12-30 10:19:17 +0100

    feat: Add a label to the comment field and enhance the display of comments

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -7,6 +7,7 @@ use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class CommentType extends AbstractType
@@ -14,7 +15,9 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
+        ->add('content', TextType::class, [
+            'label' => 'Votre commentaire :'
+        ])
             ->add('ip',  HiddenType::class, [
                 'data' => $_SERVER['REMOTE_ADDR'],
             ])
