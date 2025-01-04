1011eedb6ce566fc2beed67241c90deb0052f018
Dany Rose <danyrose1995@gmail.com>
2025-01-04 13:38:32 +0100

    feat: Add Track entity and management to admin dashboard

---
+++ src/Controller/Admin/TrackCrudController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Track;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+
+#[IsGranted('ROLE_ADMIN')]
+class TrackCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Track::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('fileName', 'Nom du fichier')
+                ->hideOnForm(),
+            TextField::new('name', 'Nom de la trace'),
+            NumberField::new('displayOrder', 'Ordre d\'affichage'),
+            TextField::new('trackFile', 'Trace GPX')
+                ->setFormType(VichFileType::class)
+                ->onlyOnForms(),
+        ];
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Trace GPX')
+            ->setEntityLabelInPlural('Traces GPX')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Liste des %entity_label_plural%')
+            ->setPageTitle(Crud::PAGE_EDIT, 'Modifier une %entity_label_singular%')
+            ->setPageTitle(Crud::PAGE_NEW, 'Ajouter une %entity_label_singular%');
+    }
+}
---
+++ src/Entity/Track.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrackRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+#[ORM\Entity(repositoryClass: TrackRepository::class)]
+#[Vich\Uploadable]
+class Track
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $fileName = null;
+
+    #[Vich\UploadableField(mapping: 'track', fileNameProperty: 'fileName', size: 'fileSize')]
+    private ?File $trackFile = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $displayOrder = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $fileSize = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFileName(): ?string
+    {
+        return $this->fileName;
+    }
+
+    public function setFileName(?string $fileName): static
+    {
+        $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    public function getTrackFile(): ?File
+    {
+        return $this->trackFile;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setTrackFile(File $trackFile): void
+    {
+        $this->trackFile = $trackFile;
+
+        if (null !== $trackFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getDisplayOrder(): ?int
+    {
+        return $this->displayOrder;
+    }
+
+    public function setDisplayOrder(?int $displayOrder): static
+    {
+        $this->displayOrder = $displayOrder;
+
+        return $this;
+    }
+
+    public function getFileSize(): ?int
+    {
+        return $this->fileSize;
+    }
+
+    public function setFileSize(?int $fileSize): static
+    {
+        $this->fileSize = $fileSize;
+
+        return $this;
+    }
+}
