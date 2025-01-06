ff0582f6c50c843c163081ed147dfa9d08e8c8f2
Cédric <c.limosin@outlook.fr>
2025-01-06 16:37:09 +0100

    fix: changed text color to white

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,7 +9,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
 
@@ -18,27 +17,32 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-    ->add('nickname', TextType::class, [
-        'label' => 'Pseudo',
-    ])
-    ->add('profilePicture', FileType::class, [
-        'label' => ' ',
-        'required' => false,
-        'mapped' => false,
-        'attr' => ['accept' => 'image/*'],
-        'constraints' => [
-            new File([
-                'maxSize' => '500K', // max size 500kb
-                'mimeTypes' => [
-                    'image/jpeg',
-                    'image/png',
-                    'image/gif',
+            ->add('nickname', TextType::class, [
+                'label' => 'Pseudo',
+                'label_attr' => [
+                    'class' => 'text-white',
                 ],
-                'mimeTypesMessage' => 'Veuillez ajouter une image aux formats valides (JPEG, PNG ou GIF)',
+                
             ])
-        ],
-    ]);
-
+            ->add('profilePicture', FileType::class, [
+                'label' => ' ',
+                'required' => false,
+                'mapped' => false,
+                'attr' => [
+                    'accept' => 'image/*',
+                ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '500K', // Taille max 500 Ko
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez ajouter une image aux formats valides (JPEG, PNG ou GIF)',
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
